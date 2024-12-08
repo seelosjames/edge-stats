1d77bf871e51b5b7c72d16419e02b7733cf41fbf
James Seelos <seelosjames@gmail.com>
2024-12-08 09:17:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -21,12 +21,22 @@ try:
     driver.get(url)
     time.sleep(5)
 
-    today_div = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/main/div/div[4]/div[2]/div')
+    # today_div = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[2]/main/div/div[4]/div[2]/div')
+    games = driver.find_elements(By.XPATH, '//*[@id="root"]/div[1]/div[2]/main/div/div[4]/div[2]/div/div')
+    for i in range(3, len(games)):
+        game = driver.find_elements(By.XPATH, '//*[@id="root"]/div[1]/div[2]/main/div/div[4]/div[2]/div/div[' + str(i) + ']')
+        print('game', game)
+        link = game.find_element(By.TAG_NAME, 'a').get_attribute("href")
+        print(link)
+    # print(len(games))
+    
+    # //*[@id="root"]/div[1]/div[2]/main/div/div[4]/div[2]/div/div[3]
+
 
     # elements = today_div.find_elements(By.TAG_NAME, "a")
     
     # print(len(elements))
-    print("Element Text:", today_div.text)
+    # print("Element Text:", today_div.text)
     # print(elements)
 
 except Exception as e:
