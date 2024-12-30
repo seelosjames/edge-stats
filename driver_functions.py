2e8e092724a9275971d38a512ce2db94e66f077a
seelosjames <97702507+seelosjames@users.noreply.github.com>
2024-12-29 19:06:28 -0700

    Added get fliff games

--- driver_functions.py
+++ driver_functions.py
@@ -4,12 +4,14 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 from webdriver_manager.chrome import ChromeDriverManager
 from betting_functions import *
+from datetime import datetime, timedelta
 import time
-from datetime import datetime
 import hashlib
 import json
+import os
 
 sports_links = {
     "nba": {
@@ -52,10 +54,66 @@ XPATH_TEAM_2 = (
 )
 
 
+def fliff_parse_datetime(input_str):
+    now = datetime.now()
+
+    if "at" in input_str:
+        if "Today" in input_str:
+            # Handle "Today at X"
+            time_str = input_str.replace("Today at ", "")
+            return datetime.strptime(time_str, "%I:%M %p").replace(
+                year=now.year, month=now.month, day=now.day
+            )
+        elif "Tomorrow" in input_str:
+            # Handle "Tomorrow at X"
+            time_str = input_str.replace("Tomorrow at ", "")
+            today_time = datetime.strptime(time_str, "%I:%M %p").replace(
+                year=now.year, month=now.month, day=now.day
+            )
+            return today_time + timedelta(days=1)
+        else:
+            # Handle "Jan 5, 2025 at X"
+            return datetime.strptime(input_str, "%b %d, %Y at %I:%M %p")
+
+    raise ValueError(f"Unsupported date string format: {input_str}")
+
+
 # Function to create a unique ID
 def generate_id(team_1, team_2, date_time_str):
     combined_string = f"{team_1}_{team_2}_{date_time_str}"
     return hashlib.md5(combined_string.encode()).hexdigest()
+
+
+# Function to load existing data from data.json
+def load_existing_data(file_path="data.json"):
+    if os.path.exists(file_path):
+        try:
+            with open(file_path, "r") as file:
+                return json.load(file)
+        except json.JSONDecodeError:
+            # If the file is empty or invalid JSON, return an empty list
+            print(f"{file_path} is empty or corrupted. Starting with an empty dataset.")
+            return []
+    return []
+
+
+# Function to save data back to data.json
+def save_data(data, file_path="data.json"):
+    with open(file_path, "w") as file:
+        json.dump(data, file, indent=4, default=str)
+
+
+# Function to update or append game data
+def update_or_add_game(data, new_game, book):
+    new_game_link = new_game['links'][book]
+    # Look for existing game by ID
+    for existing_game in data:
+        if existing_game["id"] == new_game["id"]:
+            existing_game['links'][book] = new_game_link
+            return  # Exit after updating the existing game
+    # If no match, add the new game
+    data.append(new_game)
+    print(f"Added new game with ID: {new_game['id']}")
 
 
 def get_games(
@@ -73,11 +131,11 @@ def get_games(
     ]
 ):
     # Dictionary to store matchup data
-    data = []
+    data = load_existing_data()
 
     for sport in sports:
-        get_pinnacle_games(sports_links[sport]["pinnacle"], data, sport)
-        # get_fliff_games(sports_links[sport]["fliff"])
+        # get_pinnacle_games(sports_links[sport]["pinnacle"], data, sport)
+        get_fliff_games(sports_links[sport]["fliff"], data, sport)
 
     with open("data.json", "w") as f:
         json.dump(data, f, default=str, indent=4)
@@ -143,23 +201,23 @@ def get_pinnacle_games(url, data, sport):
 
             # Parse datetime
             date_time_obj = datetime.strptime(date_time_str, "%A, %B %d, %Y at %H:%M")
+            if sport == "nba":
+                date_time_obj = date_time_obj - timedelta(minutes=10)
+
             game_id = generate_id(
                 team_1, team_2, date_time_obj.strftime("%Y-%m-%d %H:%M:%S")
             )
 
-            # Add to data if new
-            if game_id not in {entry["id"] for entry in data}:
-                data.append(
-                    {
-                        "id": game_id,
-                        "game": {
-                            "teams": [team_1, team_2],
-                            "date": date_time_obj,
-                            "league": sport,
-                        },
-                        "links": {sports_book_name: game_url},
-                    }
-                )
+            # Create new game object
+            new_game = {
+                "id": game_id,
+                "game": {"teams": [team_1, team_2], "date": date_time_obj},
+                "links": {sports_book_name: game_url},
+                "league": sport,
+            }
+
+            # Update or add the game
+            update_or_add_game(data, new_game, sports_book_name)
 
     except Exception as e:
         print(f"Error while processing link {url}: {e}")
@@ -167,20 +225,129 @@ def get_pinnacle_games(url, data, sport):
         driver.quit()
 
 
+def get_fliff_games(url, data, sport):
+    # Configure Chrome options
+    mobile_emulation = {"deviceName": "iPhone 12 Pro"}
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
+
+    # Initialize the WebDriver
+    driver = webdriver.Chrome(options=chrome_options)
+
+    sports_book_name = "fliff"
+
+    try:
+        driver.get(url)
+
+        # Wait for the games container to load
+        games = WebDriverWait(driver, 10).until(
+            EC.presence_of_all_elements_located(
+                (By.CLASS_NAME, "card-shared-container")
+            )
+        )
+
+        not_live = []
+
+        for i in range(len(games)):
+            game = games[i]
+            live = bool(game.find_elements(By.CLASS_NAME, "live-bar-state"))
+
+            if not live:
+                new_game_data = {}
+
+                new_game_data["index"] = i
+
+                date = game.find_element(
+                    By.XPATH, "div[2]/div[1]/div[1]/div/span[1]"
+                ).text
+                times = game.find_element(
+                    By.XPATH, "div[2]/div[1]/div[1]/div/span[2]"
+                ).text
+                date_time_obj = fliff_parse_datetime(date + " " + times)
+
+                team_1 = game.find_element(By.XPATH, "div[2]/div[2]/div[1]/span").text
+                team_2 = game.find_element(By.XPATH, "div[2]/div[3]/div[1]/span").text
+
+                game_id = generate_id(
+                    team_1, team_2, date_time_obj.strftime("%Y-%m-%d %H:%M:%S")
+                )
+
+                # Create new game object
+                new_game = {
+                    "id": game_id,
+                    "game": {"teams": [team_1, team_2], "date": date_time_obj},
+                    "league": sport,
+                }
+
+                new_game_data["data"] = new_game
+
+                not_live.append(new_game_data)
+
+        for game in not_live:
+            i = game["index"]
+            new_game = game["data"]
+
+            driver.get(url)
+
+            # Wait for the game containers to be loaded
+            games = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.CLASS_NAME, "card-shared-container"))
+            )
+
+            # Find the specific game card header for the game
+            card_header = games[i].find_element(By.CLASS_NAME, "card-row-header")
+            card_footer = games[i].find_element(By.CLASS_NAME, "card-regular-footer")
+
+            # Wait for the card header to be clickable
+            WebDriverWait(driver, 10).until(
+                EC.element_to_be_clickable(card_header)
+            )
+
+            # Scroll the element into view using ActionChains
+            actions = ActionChains(driver)
+            actions.move_to_element(card_footer).perform()
+
+            # Attempt a regular click
+            try:
+                card_header.click()
+                print("CORRECT CLICK")
+            except Exception as e:
+                print(f"Click failed, trying JavaScript click: {e}")
+                # Use JavaScript to click the element if regular click fails
+                driver.execute_script("arguments[0].click();", card_header)
+
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located(
+                    (By.XPATH, '//*[@id="root"]/div[1]')
+                )
+            )
+            new_game["links"] = {sports_book_name: driver.current_url}
+
+            print(new_game)
+
+            # Update or add the game
+            update_or_add_game(data, new_game, sports_book_name)
+
+        driver.quit()
+
+    except Exception as e:
+        print("An error occurred while fetching matchups:", e)
+
+
 # Example usage
 if __name__ == "__main__":
     get_games(
         [
             "nba",
-            "nfl",
-            "nhl",
-            "ncaaf",
-            "ncaab",
-            "epl",
-            "bundesliga",
-            "ligue_1",
-            "serie_a",
-            "laliga",
+            # "nfl",
+            # "nhl",
+            # "ncaaf",
+            # "ncaab",
+            # "epl",
+            # "bundesliga",
+            # "ligue_1",
+            # "serie_a",
+            # "laliga",
         ]
     )
 
