d7fe371ea4927ee1dbec19c247662d2607b32070
seelosjames <97702507+seelosjames@users.noreply.github.com>
2025-07-15 18:03:25 -0600

    Added Backend

---
+++ backend/EdgeStats/EdgeStats/Data/Sportsbook.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentMode.DataAnnotations;
+
+namespace EdgeStats.Data
+{
+    public class Sportsbook
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string SportsbookName { get; set; }
+    }
+}
